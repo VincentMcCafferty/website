e927bd142670bf570ebcc82bcf8e5e151295b6d6
Vincent <vincentmccafferty0@gmail.com>
2023-03-15 00:49:40 +0000

    Added tests for health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+//! src/main.rs
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use std::net::TcpListener;
+use website::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
-
